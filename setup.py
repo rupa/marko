ce92f55b45758a36f6d779c892f3eb849ca88eec
rupa <rupa@lrrr.us>
2011-02-13 15:09:40 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+setup(name='marko',
+      version='1.0',
+      description='bog simple markov',
+      author='rupa',
+      author_email='rupa@lrrr.us',
+      url='https://github.com/rupa/marko',
+      py_modules=['marko'],
+      license='BSD',
+      download_url="https://github.com/rupa/marko/tarball/master",
+     )
